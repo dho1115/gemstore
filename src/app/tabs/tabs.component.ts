d0c3689455b3e43de0ed6864f3b3d9b4765175f8
DAVID HO <davidhorph@hotmail.com>
2018-07-17 14:56:58 -0500

    On TabsComponents.ts
    I set the tab value to = 1 (error went away).

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -9,13 +9,14 @@ import {GemModel} from '../gem-model'
 export class TabsComponent implements OnInit {
 
 
-  @Input()
-  gem: GemModel;
+  @Input() gem: GemModel;
+  tab: number;
   setTab(selectedTab: number) {
     this.tab = selectedTab;
   };
 
   constructor() { }
+  
   ngOnInit() {
     this.tab = 1;
   }
